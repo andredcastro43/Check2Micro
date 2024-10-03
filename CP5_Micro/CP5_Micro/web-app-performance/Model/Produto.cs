8a7c1cc3644ed62b279bfce962a5d207c2bc942c
André C. <75106233+andredcastro43@users.noreply.github.com>
2024-10-03 18:56:26 -0300

    Add files via upload

---
+++ CP5_Micro/CP5_Micro/web-app-performance/Model/Produto.cs
@@ -0,0 +1,12 @@
+﻿namespace web_app_performance.Model
+{
+    public class Produto
+    {
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int QuantidadeEstoque { get; set; }
+        public string DataCriacao { get; set; }
+    }
+}
