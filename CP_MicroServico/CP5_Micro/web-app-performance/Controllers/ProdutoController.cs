dfbbdda7bded2eff7cf4710a3d3343565923de7f
André C. <75106233+andredcastro43@users.noreply.github.com>
2024-10-03 18:59:06 -0300

    Add files via upload

---
+++ CP_MicroServico/CP5_Micro/web-app-performance/Controllers/ProdutoController.cs
@@ -0,0 +1,108 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using web_app_performance.Model;
+
+namespace web_app_performance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+
+        private static ConnectionMultiplexer redis;
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduto()
+        {
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyExpireAsync(key, TimeSpan.FromSeconds(10));
+            string user = await db.StringGetAsync(key);
+
+            if (!string.IsNullOrEmpty(user))
+            {
+                return Ok(user);
+            }
+
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+            string query = "select Id, Nome, Preco, QuantidadeEstoque, DataCriacao from produtos;";
+            var produtos = await connection.QueryAsync<Produto>(query);
+            string produtosJson = JsonConvert.SerializeObject(produtos);
+            await db.StringSetAsync(key, produtosJson);
+
+            return Ok(produtos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Produto produto)
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            string sql = @"insert into produtos(nome, preco, quantidadeEstoque, dataCriacao) 
+                            values(@nome, @preco, @quantidadeEstoque, NOW());";
+            await connection.ExecuteAsync(sql, produto);
+
+            //apagar o cachê
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Produto produto)
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            string sql = @"update produtos 
+                            set Nome = @nome, 
+	                            Preco = @preco,
+                                QuantidadeEstoque = @quantidadeEstoque,
+                                DataCriacao = @dataCriacao
+                            where Id = @id;";
+
+            await connection.ExecuteAsync(sql, produto);
+
+            //apagar o cachê
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
+            using var connection = new MySqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            string sql = @"delete from produtos where Id = @id;";
+
+            await connection.ExecuteAsync(sql, new { id });
+
+            //apagar o cachê
+            string key = "getusuario";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+    }
+}
